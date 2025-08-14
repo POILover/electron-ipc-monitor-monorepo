d77f326f63668c203b4c867561ff8525ab0c31c3
wanglianjie <312822458@qq.com>
2025-08-14 13:44:06 +0800

    refactor: remove dependency on window title

--- packages/electron-ipc-monitor/main/index.ts
+++ packages/electron-ipc-monitor/main/index.ts
@@ -2,15 +2,7 @@ import { app, globalShortcut, ipcMain, BrowserWindow, IpcMainInvokeEvent } from 
 import { performance as perfHooksPerformance } from 'perf_hooks';
 import path from 'path'
 const polyfill_perf = polyfillPerformance();
-const MonitorBrowserWindowTitle = 'MONITOR'
 const shortcut = 'CmdOrCtrl+Shift+M'
-const generateBrowserWindowTitle = (parentWindowId: number) => {
-  return `${MonitorBrowserWindowTitle}_${parentWindowId}`
-}
-const resolveBrowserWindowTitle = (id: string) => {
-  const [title, parentWindowId] = id.split('_')
-  return { title, parentWindowId: Number(parentWindowId) }
-}
 export default class IpcMonitor {
   private static instance: IpcMonitor;
   private initialId = 0
@@ -24,35 +16,48 @@ export default class IpcMonitor {
   private setupWindowLifecycle() {
     app.on('browser-window-created', (_, win) => {
       const id = win.id
-      const resolvedTitle = resolveBrowserWindowTitle(win.title)
-      if (resolvedTitle.title === MonitorBrowserWindowTitle) {
-        // 说明打开的是监控窗口, id是monitor window的id
-        this.monitorWindowMap.set(resolvedTitle.parentWindowId, win)
-        win.on('closed', () => {
-          this.monitorWindowMap.delete(resolvedTitle.parentWindowId)
-        })
-        return
-      } else {
-        // 说明是业务窗口, id是monitor window的父窗口id
-        win.on('focus', () => {
+      win.on('focus', () => {
+        const isMonitorWindow = [...this.monitorWindowMap.values()].includes(win)
+        if(!isMonitorWindow){
           globalShortcut.register(shortcut, () => {
-            this.openMonitorWindow(id)
+            const hasMonitorWindow = this.monitorWindowMap.has(id) // it should define in here
+            if(hasMonitorWindow){
+              const monitorWindow = this.monitorWindowMap.get(id)
+              monitorWindow?.close()
+            }else{
+              const monitorWindow = createMonitorWindow()
+              this.monitorWindowMap.set(id, monitorWindow)
+              monitorWindow.on('closed', () => {
+                this.monitorWindowMap.delete(id)
+              })
+            }
           })
-        })
+        }
+      })
 
-        win.on('blur', () => globalShortcut.unregister(shortcut))
-        win.on('closed', () => {
+      win.on('blur', () => {
+        const isMonitorWindow = [...this.monitorWindowMap.values()].includes(win)
+        if (!isMonitorWindow) {
           globalShortcut.unregister(shortcut)
-        })
-      }
+        }
+      })
+
+      win.on('closed', () => {
+        const isMonitorWindow = [...this.monitorWindowMap.values()].includes(win)
+        if (!isMonitorWindow) {
+          const monitorWindow = this.monitorWindowMap.get(id)
+          monitorWindow?.close()
+          globalShortcut.unregister(shortcut)
+        }
+      })
+      
     })
   }
-  private getId() {
+
+  private generateIpcId() {
     return Date.now().toString() + '-' + this.initialId++
   }
-  private openMonitorWindow(parentWindowId: number) {
-    createMonitorWindow(parentWindowId)
-  }
+
   public wrapIpc() {
     const self = this
     return function (
@@ -64,13 +69,13 @@ export default class IpcMonitor {
         const wc = self.monitorWindowMap.get(parentWindowId)?.webContents
         let result: any
         if (wc) {
-          const id = self.getId();
+          const id = self.generateIpcId();
           wc.send('monitor:data', { id, channel, status: 'pending', args, perf: polyfill_perf.now() })
           try {
             result = await listener(event, ...args)
             wc.send('monitor:data', { id, channel, status: 'fullfilled', args, perf: polyfill_perf.now(), result })
           } catch (error) {
-            wc.send('monitor:data', { id, channel, status: 'rejected', args, perf: polyfill_perf.now() })
+            wc.send('monitor:data', { id, channel, status: 'rejected', args, perf: polyfill_perf.now(), result: error })
           }
         } else {
           result = await listener(event, ...args)
@@ -91,9 +96,8 @@ export default class IpcMonitor {
 export const ipcMonitor = IpcMonitor.getInstance()
 export const ipcMonitorHandle = ipcMonitor.wrapIpc()
 
-function createMonitorWindow(targetWindowId: number) {
+function createMonitorWindow() {
   const monitorWindow = new BrowserWindow({
-    title: generateBrowserWindowTitle(targetWindowId),
     width: 1200,
     height: 800,
     autoHideMenuBar: true,
@@ -102,8 +106,6 @@ function createMonitorWindow(targetWindowId: number) {
       contextIsolation: false
     }
   })
-  // 打开devtool
-  // monitorWindow.webContents.openDevTools()
 
   if (process.env.MONITOR_UI_DEV_URL) {
     console.log(`Loading monitor UI from: ${process.env.MONITOR_UI_DEV_URL}`)
@@ -112,10 +114,10 @@ function createMonitorWindow(targetWindowId: number) {
     const moduleRoot = path.dirname(require.resolve('electron-ipc-monitor'))
     const uiDistPath = path.resolve(moduleRoot, 'ui/index.html')
     monitorWindow.loadFile(uiDistPath).catch((err) => {
-      // fallback: 兼容开发环境或路径不对时的报错
       console.error('Failed to load monitor UI:', err)
     })
   }
+  return monitorWindow;
 }
 
 function polyfillPerformance() {
